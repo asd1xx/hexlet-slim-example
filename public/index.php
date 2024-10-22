21816c46b855e3684a1b728a48bd017302031be6
asd2d <872122@mail.ru>
2024-10-22 16:18:52 +0300

    added a dynamic route handler

--- public/index.php
+++ public/index.php
@@ -19,4 +19,13 @@ $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
+
 $app->run();
