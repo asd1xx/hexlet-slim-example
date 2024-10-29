33f60c2201271da8d0b61a04b10a86d6e1f3e0da
asd2d <872122@mail.ru>
2024-10-29 21:34:20 +0300

    array users structure changed

--- public/index.php
+++ public/index.php
@@ -6,7 +6,14 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
-$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+$users = [
+    ['id' => 'user67212379cdf1a', 'nickname' => 'max', 'email' => '872122@mail.ru'],
+    ['id' => 'user672124b492c4b', 'nickname' => 'roman', 'email' => 'fan@mail.ru']
+];
+$data = file_get_contents('users.json');
+$toArray = json_decode($data, true);
+$users[] = $toArray;
+
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
@@ -14,6 +21,7 @@ $container->set('renderer', function () {
 });
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+$router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
@@ -25,11 +33,11 @@ $app->get('/', function ($request, $response) {
 $app->get('/users', function ($request, $response) use ($users) {
     $term = $request->getQueryParam('term');
     $filteredUsers = array_filter($users, function($user) use ($term) {
-        return str_contains($user, $term) === true;
+        return str_contains($user['nickname'], $term) === true;
     });
     $params = ['users' => $filteredUsers, 'term' => $term];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users');
 
 $app->get('/users/new', function ($request, $response) {
     $params = [
@@ -39,9 +47,12 @@ $app->get('/users/new', function ($request, $response) {
 });
 
 $app->post('/users', function ($request, $response) {
-    $user = $request->getParsedBodyParam('user');
+    $dataRequest = $request->getParsedBodyParam('user');
+    $id = ['id' => uniqid('user')];
+    $user = array_merge($id, $dataRequest);
     $file = 'users.json';
-    file_put_contents($file, json_encode($user), FILE_APPEND | LOCK_EX);
+    // file_put_contents($file, json_encode($user), FILE_APPEND | LOCK_EX);
+    file_put_contents($file, json_encode($user));
     return $response->withRedirect('/users', 302);
     $params = [
         'user' => $user
