a11478c932f7090f6dc6a53d86adbfdfb7f969ff
asd2d <872122@mail.ru>
2024-10-31 21:11:30 +0300

    implemented saving new users into valid json

--- public/index.php
+++ public/index.php
@@ -6,24 +6,30 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
-$users = [
-    ['id' => 'user67212379cdf1a', 'nickname' => 'max', 'email' => '872122@mail.ru'],
-    ['id' => 'user672124b492c4b', 'nickname' => 'roman', 'email' => 'fan@mail.ru']
-];
-$data = file_get_contents('users.json');
-$toArray = json_decode($data, true);
-$users[] = $toArray;
+const USER_LIST = 'users.json';
+
+$data = file_get_contents(USER_LIST);
+$users = json_decode($data, true);
 
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 $router = $app->getRouteCollector()->getRouteParser();
 
-$app->get('/', function ($request, $response) {
+$app->get('/', function ($request, $response) use ($router) {
+    $router->urlFor('users');
+    $router->urlFor('new_users');
+    $router->urlFor('create');
+    $router->urlFor('course', ['id' => 1]);
+    $router->urlFor('user', ['id' => 1]);
     $response->getBody()->write('Welcome to Slim!');
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
@@ -35,6 +41,8 @@ $app->get('/users', function ($request, $response) use ($users) {
     $filteredUsers = array_filter($users, function($user) use ($term) {
         return str_contains($user['nickname'], $term) === true;
     });
+    // $messages = $this->get('flash')->getMessages();
+    // $params = ['users' => $filteredUsers, 'term' => $term, 'flash' => $messages];
     $params = ['users' => $filteredUsers, 'term' => $term];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('users');
@@ -43,34 +51,49 @@ $app->get('/users/new', function ($request, $response) {
     $params = [
         'user' => ['nickname' => '', 'email' => '']
     ];
-    return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
+    return $this->get('renderer')->render($response, "users/new.phtml");
+})->setName('new_users');
 
 $app->post('/users', function ($request, $response) {
     $dataRequest = $request->getParsedBodyParam('user');
     $id = ['id' => uniqid('user')];
-    $user = array_merge($id, $dataRequest);
-    $file = 'users.json';
-    // file_put_contents($file, json_encode($user), FILE_APPEND | LOCK_EX);
-    file_put_contents($file, json_encode($user));
-    return $response->withRedirect('/users', 302);
+    $user[] = array_merge($id, $dataRequest);
+
+    if (!file_exists(USER_LIST) || filesize(USER_LIST) === 0) {
+        file_put_contents(USER_LIST, json_encode($user));
+    } else {
+        $dataFile = json_decode(file_get_contents(USER_LIST), true);
+        $dataResult = array_merge($dataFile, $user);
+        file_put_contents(USER_LIST, json_encode($dataResult));
+    }
+
+    // $this->get('flash')->addMessage('success', 'OK');
+    // return $response->withRedirect('/users');
     $params = [
         'user' => $user
     ];
-    return $this->get('renderer')->render($response, "users/new.phtml", $params);
-});
+    return $this->get('renderer')->render($response->withRedirect('/users'), "users/new.phtml", $params);
+})->setName('create');
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
-});
+})->setName('course');
 
-$app->get('/users/{id}', function ($request, $response, array $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
-    // $this в Slim это контейнер зависимостей
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+$app->get('/users/{id}', function ($request, $response, array $args) use ($users) {
+    $id = $args['id'];
+
+    foreach ($users as $user) {
+        if ($user['id'] === $id) {
+            $params = ['id' => $args['id'], 'nickname' => $user['nickname'], 'email' => $user['email']];
+            // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+            // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+            // $this в Slim это контейнер зависимостей
+            return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+        }
+    }
+    
+    return $response->withStatus(404);
+})->setName('user');
 
 $app->run();
